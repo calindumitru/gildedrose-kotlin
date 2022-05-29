23729c68c91f17aaefa2726860fc1ac0fe5e7aa1
calin.dumitru <calin.dumitru@creactives.com>
2022-05-29 14:47:46 +0200

    added regression tests and refactored solution to use separate calculation/update functions for each item type.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.10"
 }
 
 group = "com.gildedrose"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 }
 
 tasks.test {
-	useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
-	}
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 // config JVM target to 1.8 for kotlin compilation tasks
 tasks.withType<KotlinCompile>().configureEach {
-	kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "1.8"
 }
--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -1,7 +1,84 @@
 package com.gildedrose
 
+fun Item.updateGeneric() {
+    decreaseQuality()
+    sellIn--
+    if (sellIn < 0) {
+        decreaseQuality()
+    }
+}
+
+fun Item.updateAgedBrie() {
+    increaseQuality()
+    sellIn--
+    if (sellIn < 0) {
+        increaseQuality()
+    }
+}
+
+fun Item.updateLegendaryItem() {
+    sellIn--
+    quality = 80
+}
+
+fun Item.increaseQuality(amount: Int = 1) {
+    quality = (quality + amount).coerceAtMost(50)
+}
+
+fun Item.decreaseQuality(amount: Int = 1) {
+    quality = (quality - amount).coerceAtLeast(0)
+}
+
+fun Item.updateBackstagePass() {
+    increaseQuality()
+    if (sellIn <= 10) {
+        increaseQuality()
+    }
+    if (sellIn <= 5) {
+        increaseQuality()
+    }
+    sellIn--
+    if (sellIn < 0) {
+        quality = 0
+    }
+}
+
+fun Item.updateConjuredItem() {
+    decreaseQuality(2)
+    sellIn--
+    if (sellIn < 0) {
+        decreaseQuality(2)
+    }
+}
+
+fun Item.toNew(): FNItem {
+    return when {
+        name == "Aged Brie" -> {
+            FNItem(this, Item::updateAgedBrie)
+        }
+        name == "Sulfuras, Hand of Ragnaros" -> {
+            require(quality == 80)
+            FNItem(this, Item::updateLegendaryItem)
+        }
+        name.contains("Backstage pass") -> {
+            FNItem(this, Item::updateBackstagePass)
+        }
+        name.contains("Conjured") -> {
+            FNItem(this, Item::updateConjuredItem)
+        }
+        else -> FNItem(this, Item::updateGeneric)
+    }
+}
+
 open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
+    }
+}
+
+typealias UpdateQuality = Item.() -> Unit
+class FNItem(private val item: Item, private val updateQuality: UpdateQuality) {
+    fun update() {
+        this.item.updateQuality()
     }
 }
